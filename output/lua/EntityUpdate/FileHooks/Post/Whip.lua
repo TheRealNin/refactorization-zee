a1d6ff74d3f049b0641e2666240793f0606ddb3b
Nin <therealnin@users.noreply.github.com>
2018-07-05 11:28:40 -0400

    Bypass optimization for whips

--- output/lua/EntityUpdate/FileHooks/Post/Whip.lua
+++ output/lua/EntityUpdate/FileHooks/Post/Whip.lua
@@ -2,4 +2,11 @@ if kUseFixedUpdates then
     function Whip:GetTickTime()
         return 0 -- realtime
     end
+    
+    local oldOnInitialized = Whip.OnInitialized
+    function Whip:OnInitialized()
+        oldOnInitialized(self)
+        self:SetUpdates(false)
+        self:SetUpdatesActual(true)
+    end
 end
