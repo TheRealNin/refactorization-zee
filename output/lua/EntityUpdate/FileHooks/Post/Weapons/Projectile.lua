8aa12185a20b5e942d59ca8a0ca55c20fbae2915
Nin <therealnin@users.noreply.github.com>
2018-07-05 12:13:34 -0400

    Fix a vanilla bug that was hidden by slow updates

--- output/lua/EntityUpdate/FileHooks/Post/Weapons/Projectile.lua
+++ output/lua/EntityUpdate/FileHooks/Post/Weapons/Projectile.lua
@@ -3,3 +3,9 @@ if kUseFixedUpdates then
         return 0 -- realtime
     end
 end
+
+local oldAdjustInitial = Projectile.AdjustInitial
+function Projectile:AdjustInitial(setup)
+    oldAdjustInitial(self, setup)
+    self.lastOrigin = self:GetOrigin()
+end
